eae27ddbe13db8b8b10e294afc6b143305a0b83e
massillia65 <martinez_65@hotmail.fr>
2016-11-03 11:12:13 +0100

    #1

---
+++ src/main/java/lmar/ItemPerso.java
@@ -0,0 +1,12 @@
+package lmar;
+
+/**
+ * Created by 21204603 on 03/11/2016.
+ */
+public class ItemPerso extends Item {
+
+    public ItemPerso(Object valeur)
+    {
+        super(valeur);
+    }
+}
